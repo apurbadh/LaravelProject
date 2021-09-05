0d292973feffedc4c09ca1be34e9ebf3af24e9fe
apurbadh <erikduke182@gmail.com>
2021-09-05 22:01:20 +0545

    Assignment Done

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i < 51; $i++){
+            DB::table('students')->insert([
+                "name" => Str::random(12),
+                "email" => Str::random(8)."@gmail.com",
+                "phone" => $i * 111111111,
+                "password" => Hash::make(Str::random(8))
+            ]);
+        }
+    }
+}
